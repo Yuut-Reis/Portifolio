6a81535f775ac531014a9dc6ab371a69f7d18620
Yuut <ingride.reis.b@gmail.com>
2022-06-19 11:26:29 -0300

    CSS e HTML da sessão de skills concluidas

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const iconeHamburguer = document.querySelector('.icone-hamburger')
+const iconeHamburguer = document.querySelector('.icone-hamburger');
+const carrosel = document.querySelector('.carrosel');
+
 const hamburguerTogle = () => {
     const menu = document.querySelector('.menu');
     menu.classList.toggle('open');
@@ -6,7 +8,14 @@ const hamburguerTogle = () => {
 
 const lightDark = () => {
    const icon = document.querySelector(".dark-light")
-   
+}
+
+const carroselScroll = (verify) => {
+    if(verify){
+     carrosel.scrollBy(300,0)
+    }else if (!verify) {
+        carrosel.scrollBy(-300, 0)
+    }   
 }
 
 iconeHamburguer.addEventListener('click',hamburguerTogle);
